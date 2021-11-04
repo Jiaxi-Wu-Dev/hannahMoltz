d6fa061e6b7379d5f3b3a82ec03b40a447a72589
Jiaxi <dev.jiaxiwu@gmail.com>
2021-11-03 22:41:14 -0500

    module component

--- src/App.js
+++ src/App.js
@@ -3,9 +3,16 @@ import './App.css';
 import Window from './components/Window'
 
 function App() {
+  
+  const firstWindowStyles = {
+    position:"relative", 
+    top:"-60px",
+    left:"35px",
+    zIndex:"1"
+  }
   return (
     <div className="App">
-      <Window className="firstWindow" number={"1"}/>
+      <Window className="firstWindow" number={"1"} style={firstWindowStyles}/>
       <Window id="secondWindow" number={"2"}/>
       <Window id="thirdWindow" number={"3"}/>
     </div>
